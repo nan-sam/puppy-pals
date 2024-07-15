cdd5cf37b6e63af13aa770887d40b0dbfaf6b8bb
Nange Sam <nangesam1@gmail.com>
2024-07-15 14:41:07 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
-import './App.css';
+import {useState} from "react";
+import "./App.css";
+import { puppyList } from "./data";
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((puppy) => puppy.id===featPupId);
 
-  return <>APP</>;
-}
+  return (
+    <>
+      {featPupId && (
+     <div>
+      <h2>{featuredPup.name}</h2>
+      <ul>
+        <li>Age: {featuredPup.age}</li>
+        <li>Email: {featuredPup.email}</li>
+      </ul>
+      </div>
+      )}
+      {puppies.map((puppy) => (
+        <p
+        key={puppy.id}
+        onClick={() => {
+          setFeatPupId(puppy.id);
+        }}
+         >
+            {puppy.name}
+          </p>
+        ))}
+        </>
+  );
 
-export default App
+      }
+      
+
+export default App;
